23a9c8ccbf000680d327ba0e9f23b5d08b495c76
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2015-04-08 10:59:34 +0200

    Analysis for 5 arc min results, non-natural, 27 march 2015.

--- 0_evaluation_to_grace.py
+++ 0_evaluation_to_grace.py
@@ -21,13 +21,13 @@ import virtualOS as vos
 input_files = {}
 #
 # - total thickness of water storage, from PCR-GLOBWB
-model_output_folder                                          = '/scratch/edwin/05min_runs_results/2015-03-27_and_2015-04-01/non-natural_2015-04-01/global/'
-input_files["model_total_water_storage"]                     = model_output_folder+'netcdf/totalWaterStorageThickness_monthAvg_output.nc' # unit: meter
+model_output_folder                                          = '/scratch/edwin/05min_runs_results/2015-03-27_and_2015-04-01/non-natural_2015-03-27/global/'
+input_files["model_total_water_storage"]                     = model_output_folder+'/netcdf/totalWaterStorageThickness_monthAvg_output_2000to2010.nc'               # unit: meter
 input_files["model_total_water_storage_variable_name"]       = "total_thickness_of_water_storage"
 #
 # - cell area for the model (unit: m2, depending on PCR-GLOBWB resolution)
-input_files["model_cell_area"]                            = '/data/hydroworld/PCRGLOBWB20/input5min/routing/cellsize05min.correct.map' 
-#~ input_files["model_cell_area"]                               = '/data/hydroworld/PCRGLOBWB20/input30min/routing/cellarea30min.map' 
+input_files["model_cell_area"]                               = '/data/hydroworld/PCRGLOBWB20/input5min/routing/cellsize05min.correct.map' 
+#~ input_files["model_cell_area"]                            = '/data/hydroworld/PCRGLOBWB20/input30min/routing/cellarea30min.map' 
 #
 # - catchment/basin/aquifer classification (please provide the input map in a 30 arc-minute resolution)  
 #~ input_files["basin30minmap"]                              = '/scratch/edwin/input/basin_and_grace_from_yoshi/globalcat.map' 
