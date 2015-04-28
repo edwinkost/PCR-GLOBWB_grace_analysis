aba3d6e5ec68498da85f67f22a216a13092ddae3
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-04-28 21:43:20 +0200

    Analyzing 30min results (major aquifer).

--- 0_evaluation_to_grace.py
+++ 0_evaluation_to_grace.py
@@ -31,8 +31,8 @@ input_files["model_total_water_storage_variable_name"]       = "total_thickness_
 input_files["model_cell_area"]                               = '/data/hydroworld/PCRGLOBWB20/input30min/routing/cellarea30min.map' 
 #
 # - catchment/basin/aquifer classification (please provide the input map in a 30 arc-minute resolution)  
-input_files["basin30minmap"]                                 = '/scratch/edwin/input/basin_and_grace_from_yoshi/globalcat.map' 
-#~ input_files["basin30minmap"]                              = '/home/sutan101/data/processing_whymap/version_19september2014/major_aquifer_30min.extended.map'
+#~ input_files["basin30minmap"]                              = '/scratch/edwin/input/basin_and_grace_from_yoshi/globalcat.map' 
+input_files["basin30minmap"]                                 = '/home/sutan101/data/processing_whymap/version_19september2014/major_aquifer_30min.extended.map'
 #~ input_files["basin30minmap"]                              = '/home/sutan101/data/data_from_jt/mask_nominal_halfdegree.map'
 # - cell area for the catchment/basin/aquifer classification (unit: m2, 30 arc-minute resolution)  
 input_files["area30min_map"]                                 = '/data/hydroworld/PCRGLOBWB20/input30min/routing/cellarea30min.map' 
@@ -45,7 +45,7 @@ input_files["one_degree_id"]                                 = '/data/hydroworld
                                                              
 # output files:                                              
 output_files = {}                                            
-output_files['output_folder']                                = model_output_folder+"/analysis/grace/basin/"
+output_files['output_folder']                                = model_output_folder+"/analysis/grace/major_aquifer/"
 #~ output_files['output_folder']                             = "/scratch/edwin/05min_runs_results/2015-03-27_and_2015-04-01/non-natural_2015-04-01/global/analysis/grace/"
 #~ output_files['output_folder']                             = "/scratch/edwin/test_grace/"
 cleanOutputFolder = True                                     
